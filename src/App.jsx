ef80d089e00b98c076b2a30a6e38933aa25efa10
04ale <ale.pereira0422@gmail.com>
2025-08-15 17:53:00 -0300

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
   const { currentUser, loading } = useAuth();
@@ -20,11 +21,25 @@ function App() {
 
   return (
     <div>
-    <Routes>
-      <Route path="/" element={currentUser ? <Home /> : <Navigate to='/login' />} />
-      <Route path="/login" element={!currentUser ? <Login /> : <Navigate to='/' />} />
-      <Route path="/register" element={!currentUser ? <Register />: <Navigate to='/' />} />
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={currentUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={currentUser ? <Profile /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="/login"
+          element={!currentUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!currentUser ? <Register /> : <Navigate to="/" />}
+        />
+      </Routes>
     </div>
   );
 }
