a73fb005b84b9e122d46193b45b1d6f40c718c11
04ale <ale.pereira0422@gmail.com>
2025-08-22 11:44:36 -0300

    commit id

---
+++ src/components/Post.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import {
+  arrayUnion,
+  doc,
+  increment,
+  updateDoc,
+  arrayRemove,
+} from "firebase/firestore";
+import { db } from "../services/FirebaseConfig";
+import { Heart, User } from "lucide-react";
+import { auth } from "../services/FirebaseConfig";
+import { useAuth } from "../Authentication";
+import { useParams } from "react-router-dom";
+
+function Post({ postData }) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(postData.likesCount);
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (currentUser && postData.likedBy?.includes(currentUser.uid)) {
+      setIsLiked(true);
+    } else {
+      setIsLiked(false);
+    }
+  }, [postData.likedBy, currentUser]);
+
+  useEffect(()=> {
+    const userRef = doc(db, 'users', users.username)
+  }, [])
+  
+
+  const handleLike = async () => {
+    const postDocRef = doc(db, "posts", postData.id);
+    const newIsLiked = !isLiked;
+
+    try {
+      await updateDoc(postDocRef, {
+        likesCount: increment(newIsLiked ? 1 : -1),
+        likedBy: newIsLiked
+          ? arrayUnion(currentUser.uid)
+          : arrayRemove(currentUser.uid),
+      });
+      setIsLiked(newIsLiked);
+      setLikesCount((prevCount) =>
+        newIsLiked ? prevCount + 1 : prevCount - 1
+      );
+    } catch (err) {
+      console.log("Erro ao curtir o post", err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-12">
+      <div key={postData.id} className="flex flex-col gap-4">
+        <div className="flex gap-2 pl-4 items-center">
+          <Link to={`/userprofile/${users.id}`}>
+            {postData.profilePicUrl ? (
+              <img
+                src={postData.profilePicUrl}
+                className="rounded-full h-10 w-10"
+              />
+            ) : (
+              <User className="rounded-2xl bg-black text-white p-1" />
+            )}
+
+            <p className="font-semibold">{postData.username}</p>
+          </Link>
+        </div>
+        <img
+          onDoubleClick={handleLike}
+          src={postData.imageUrl}
+          className="w-full aspect-square md:border-1 md:border-gray-200 md:rounded-lg"
+        />
+        {isLiked === false ? (
+          <div className="flex flex-row gap-2">
+            <Heart className="ml-4 cursor-pointer" onClick={handleLike} />
+            <p className="font-bold">{postData.likesCount}</p>
+          </div>
+        ) : (
+          <div className="flex flex-row gap-2">
+            <Heart
+              className="ml-4 fill-red-600 text-red-600 cursor-pointer"
+              onClick={handleLike}
+            />
+            <p className="font-bold">{postData.likesCount + 1}</p>
+          </div>
+        )}
+
+        <p className="pl-4">
+          <span className="font-bold">{postData.username}</span>:{" "}
+          {postData.description}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
